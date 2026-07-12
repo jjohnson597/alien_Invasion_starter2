dd7bd57c06c57c42b3809828e97844691b3dac8e
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-07-12 18:50:18 -0400

    Add: Movement (left and right) Modified: Settings

--- settings.py
+++ settings.py
@@ -8,7 +8,10 @@ class Settings:
         self.screen_height = 800
         self.FPS = 60
         self.bg_file = pathlib.Path.cwd() / "Assets" / "images" / "Starbasesnow.png"
+        
+        self.ship_file = pathlib.Path.cwd() / "Assets" / "images" / "ship2(no bg).png"
         self.ship_width = 40
         self.ship_height = 60
-        self.ship_speed = 1.5
-        self.ship_file = pathlib.Path.cwd() / "Assets" / "images" / "ship2(no bg).png"
+        self.ship_speed = 5
+        
+
