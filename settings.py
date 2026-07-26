85edd107e6b39707fbd76931622ae00b859e1bce
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-07-26 12:16:15 -0400

    Split settings into dynamic and static added increase difficulty

--- settings.py
+++ settings.py
@@ -12,24 +12,16 @@ class Settings:
         self.ship_file = pathlib.Path.cwd() / "Assets" / "images" / "ship2(no bg).png"
         self.ship_width = 40
         self.ship_height = 60
-        self.ship_speed = 5
-        self.ship_limit = 3
 
         self.bullet_file = pathlib.Path.cwd() / "Assets" / "images" / "laserBlast.png"
         self.laser_sound = pathlib.Path.cwd() / "Assets" / "sound" / "laser.mp3"
         self.impact_sound = pathlib.Path.cwd()/ "Assets"/ "sound"/ "impactSound.mp3"
-        self.bullet_width = 25
-        self.bullet_height = 80
-        self.bullet_speed = 7
         self.bullet_color = (255, 0, 0)
-        self.bullet_amount = 5
 
         self.alien_file = pathlib.Path.cwd() / "Assets" / "images" / "enemy_4.png"
         self.alien_width = 40
         self.alien_height = 40
-        self.alien_speed = 2
         self.alien_direction = 1
-        self.alien_drop_speed = 40
 
         self.font_file = pathlib.Path.cwd()/ "Assets"/ "Fonts"/ "Silkscreen"/ "Silkscreen-Regular.ttf"
         self.button_width = 200
@@ -38,3 +30,25 @@ class Settings:
         self.text_color = (255, 255, 255)
         self.button_font_size = 48
         self.HUD_font_size = 20
+        self.difficulty_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change during gameplay."""
+        self.ship_speed = 5
+        self.ship_limit = 3
+
+        self.bullet_width = 25
+        self.bullet_height = 80
+        self.bullet_speed = 7
+        self.bullet_amount = 5
+
+        self.alien_speed = 2
+        self.alien_drop_speed = 40
+
+    def increase_difficulty(self):
+        """Increase the speed settings for a new level."""
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.alien_speed *= self.difficulty_scale
+        
